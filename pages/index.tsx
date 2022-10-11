60c3176c28d2057d31b586cbad963ff72fa9eb5f
YeudaBy <YeudaBorodyanski@gmail.com>
2022-10-11 23:27:07 +0300

    sco

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import {Sentence} from "../src/Sentence";
 import styles from '../styles/Main.module.scss'
 import {FaSearch} from "react-icons/fa";
 import {useRouter} from "next/router";
+import Head from "next/head";
 
 const Home: NextPage = () => {
     const [query, setQuery] = useState<string>("");
@@ -50,7 +51,46 @@ const Home: NextPage = () => {
     }, [router.query.q, router.query.s]);
 
     return (<div className={styles.home}>
-        <h2 className={styles.header}>
+        <Head>
+            <title>Friends Search - search anything from the iconic series Friends</title>
+            <meta name="description" content="Search anything from the iconic series Friends"/>
+            <meta name="keywords"
+                  content="friends, search, quotes, sentences, episodes, seasons, friends search, friends quotes, friends sentences, friends episodes, friends seasons"/>
+            <meta name="author" content="Yeuda By"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="theme-color" content="#EF4D36"/>
+            <meta name="msapplication-navbutton-color" content="#EF4D36"/>
+            <meta name="apple-mobile-web-app-status-bar-style" content="#EF4D36"/>
+            <meta name="msapplication-TileColor" content="#EF4D36"/>
+            <meta name="application-name" content="Friends Search"/>
+            <meta name="apple-mobile-web-app-title" content="Friends Search"/>
+            <meta name="msapplication-tooltip" content="Friends Search"/>
+            <meta name="msapplication-starturl" content="https://friends-search.yeudaby.com/"/>
+            <meta name="og:title" content="Friends Search"/>
+            <meta name="og:description" content="Search anything from the iconic series Friends"/>
+            <meta name="og:image" content="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends-logo_512.png"/>
+            <meta name="og:url" content="https://friends-search.yeudaby.com/"/>
+            <meta name="og:site_name" content="Friends Search"/>
+            <meta name="og:locale" content="en_US"/>
+            <meta name="og:type" content="website"/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:title" content="Friends Search"/>
+            <meta name="twitter:description" content="Search anything from the iconic series Friends"/>
+            <meta name="twitter:image" content="https://friends-search.yeudaby.com/og-image.png"/>
+            <meta name="twitter:image:alt" content="Friends Search"/>
+            <meta name="twitter:domain" content="friends-search.yeudaby.com"/>
+            <meta name="twitter:url" content="https://friends-search.yeudaby.com/"/>
+            <link rel="apple-touch-icon" href="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends-logo_512.png"/>
+            <link rel="mask-icon" href="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends-logo_512.png" color="#EF4D36"/>
+            <link rel="shortcut icon" href="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends-logo_512.png"/>
+            <link rel="canonical" href="https://friends-search.yeudaby.com/"/>
+
+            <link rel="icon" href="https://github.com/YeudaBy/Friends-Search/raw/main/web/public/friends_logo_16.ico" type="image/gif" sizes="16x16" />
+            <link rel="icon" href="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends_logo32.ico" type="image/gif" sizes="32x32" />
+            <link rel="icon" href="https://raw.githubusercontent.com/YeudaBy/Friends-Search/main/web/public/friends_logo_48.ico" type="image/gif" sizes="48x48" />
+
+        </Head>
+        <h2 className={styles.header} translate={"no"}>
             F<span className={styles.red}>&middot;</span>
             r<span className={styles.blue}>&middot;</span>
             i<span className={styles.yellow}>&middot;</span>
